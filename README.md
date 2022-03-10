ad36c91d74ab9df78632e667de8cddfee4bb317c
tm-sanjay <tmsanjay.in@gmail.com>
2022-03-10 20:55:00 +0530

    After installing usb_serial

--- README.md
+++ README.md
@@ -1,16 +1,71 @@
-# arduino_usb_serial_flutter
+# Arduino_usb_serial_flutter
 
-A new Flutter project.
+This project demonstrate the communication b/w Flutter App and Arduino(FTDI Device) via USD_Serial(UART)
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+Install [usb_serial](https://pub.dev/packages/usb_serial) package
 
-A few resources to get you started if this is your first Flutter project:
+Add a dependency to your pubspec.yaml
+```
+dependencies:
+	usb_serial: ^0.4.0
+```
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+IMPORTANT app\build.gradle
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Edit ```android\app\build.gradle``` and add
+
+```
+    compileOptions {
+        sourceCompatibility 1.8
+        targetCompatibility 1.8
+    }
+```
+
+In ```AndroidManifest.xml``` (android\app\src) add  
+```
+<intent-filter>
+		<action android:name="android.hardware.usb.action.USB_DEVICE_ATTACHED" />
+</intent-filter>
+
+<meta-data android:name="android.hardware.usb.action.USB_DEVICE_ATTACHED"
+		android:resource="@xml/device_filter" />
+
+```
+
+Create a new folder in res called ```xml```, inside xml create a file called ```device_filter.xml``` and past the following
+```
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+    <!-- 0x0403 / 0x6001: FTDI FT232R UART -->
+    <usb-device vendor-id="1027" product-id="24577" />
+    
+    <!-- 0x0403 / 0x6015: FTDI FT231X -->
+    <usb-device vendor-id="1027" product-id="24597" />
+
+    <!-- 0x2341 / Arduino -->
+    <usb-device vendor-id="9025" />
+
+    <!-- 0x16C0 / 0x0483: Teensyduino  -->
+    <usb-device vendor-id="5824" product-id="1155" />
+
+    <!-- 0x10C4 / 0xEA60: CP210x UART Bridge -->
+    <usb-device vendor-id="4292" product-id="60000" />
+    
+    <!-- 0x067B / 0x2303: Prolific PL2303 -->
+    <usb-device vendor-id="1659" product-id="8963" />
+
+    <!-- 0x1366 / 0x0105: Segger JLink -->
+    <usb-device vendor-id="4966" product-id="261" />
+
+    <!-- 0x1366 / 0x0105: CH340 JLink -->
+    <usb-device vendor-id="1A86" product-id="7523" />
+
+</resources>
+```
+
+Use the provided [exaple code](https://pub.dev/packages/usb_serial/example) 
+
+- Note : Remove this line ```subtitle: Text(device.manufacturerName!),``` if using CH340 unbranded chips
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,174 @@
+import 'dart:async';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:usb_serial/transaction.dart';
+import 'package:usb_serial/usb_serial.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class _MyAppState extends State<MyApp> {
+  UsbPort? _port;
+  String _status = "Idle";
+  List<Widget> _ports = [];
+  final List<Widget> _serialData = [];
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  StreamSubscription<String>? _subscription;
+  Transaction<String>? _transaction;
+  UsbDevice? _device;
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  final TextEditingController _textController = TextEditingController();
 
-  final String title;
+  Future<bool> _connectTo(device) async {
+    _serialData.clear();
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+    if (_subscription != null) {
+      _subscription!.cancel();
+      _subscription = null;
+    }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+    if (_transaction != null) {
+      _transaction!.dispose();
+      _transaction = null;
+    }
 
-  void _incrementCounter() {
+    if (_port != null) {
+      _port!.close();
+      _port = null;
+    }
+
+    if (device == null) {
+      _device = null;
+      setState(() {
+        _status = "Disconnected";
+      });
+      return true;
+    }
+
+    _port = await device.create();
+    if (await (_port!.open()) != true) {
+      setState(() {
+        _status = "Failed to open port";
+      });
+      return false;
+    }
+    _device = device;
+
+    await _port!.setDTR(true);
+    await _port!.setRTS(true);
+    await _port!.setPortParameters(
+        115200, UsbPort.DATABITS_8, UsbPort.STOPBITS_1, UsbPort.PARITY_NONE);
+
+    _transaction = Transaction.stringTerminated(
+        _port!.inputStream as Stream<Uint8List>, Uint8List.fromList([13, 10]));
+
+    _subscription = _transaction!.stream.listen((String line) {
+      setState(() {
+        _serialData.add(Text(line));
+        if (_serialData.length > 20) {
+          _serialData.removeAt(0);
+        }
+      });
+    });
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _status = "Connected";
+    });
+    return true;
+  }
+
+  void _getPorts() async {
+    _ports = [];
+    List<UsbDevice> devices = await UsbSerial.listDevices();
+    if (!devices.contains(_device)) {
+      _connectTo(null);
+    }
+    print(devices);
+
+    devices.forEach((device) {
+      _ports.add(ListTile(
+          leading: Icon(Icons.usb),
+          title: Text(device.productName!),
+          // subtitle: Text(device.manufacturerName!), // Remove if using CH340 unbranded driver
+          trailing: ElevatedButton(
+            child: Text(_device == device ? "Disconnect" : "Connect"),
+            onPressed: () {
+              _connectTo(_device == device ? null : device).then((res) {
+                _getPorts();
+              });
+            },
+          )));
+    });
+
+    setState(() {
+      print(_ports);
     });
   }
 
   @override
+  void initState() {
+    super.initState();
+
+    UsbSerial.usbEventStream!.listen((UsbEvent event) {
+      _getPorts();
+    });
+
+    _getPorts();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _connectTo(null);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
+    return MaterialApp(
+        home: Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('USB Serial Plugin example app'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          child: Column(children: <Widget>[
+        Text(
+            _ports.length > 0
+                ? "Available Serial Ports"
+                : "No serial devices available",
+            style: Theme.of(context).textTheme.headline6),
+        ..._ports,
+        Text('Status: $_status\n'),
+        Text('info: ${_port.toString()}\n'),
+        ListTile(
+          title: TextField(
+            controller: _textController,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Text To Send',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
+          trailing: ElevatedButton(
+            child: Text("Send"),
+            onPressed: _port == null
+                ? null
+                : () async {
+                    if (_port == null) {
+                      return;
+                    }
+                    String data = _textController.text + "\r\n";
+                    await _port!.write(Uint8List.fromList(data.codeUnits));
+                    _textController.text = "";
+                  },
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        Text("Result Data", style: Theme.of(context).textTheme.headline6),
+        ..._serialData,
+      ])),
+    ));
   }
 }
